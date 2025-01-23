ed52d027b392dfdc048f26a1152be1262bfaec03
Charly2312 <charlychandra99@gmail.com>
2025-01-24 00:58:31 +0800

    change the code to greeting and bye. New chatbot name is Aples

---
+++ src/main/java/Aples.java
@@ -0,0 +1,19 @@
+public class Aples {
+    public static void main(String[] args) {
+        /*String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println(logo);*/
+
+        int length = 50; // Length of the horizontal line
+
+        System.out.println("-".repeat(length));
+        System.out.println("Hello! I'm " + "Aples");
+        System.out.println("What can I do for you?");
+        System.out.println("-".repeat(length));
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("-".repeat(length));
+    }
+}
