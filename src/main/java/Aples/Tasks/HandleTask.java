5ca128f2e28f802ac5116c9008c8d64db265eefc
Charly2312 <charlychandra99@gmail.com>
2025-02-13 19:30:27 +0800

    Handled errors for empty description and if no keyword is detected

---
+++ src/main/java/Aples/Tasks/HandleTask.java
@@ -0,0 +1,5 @@
+package Aples.Tasks;
+
+public class HandleTask {
+
+}
---
+++ src/main/java/Aples/Tasks/Task.java
@@ -0,0 +1,32 @@
+package Aples.Tasks;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String task) {
+        this.description = task;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markNotDone() {
+        this.isDone = false;
+    }
+
+    public String getSign() {
+        return (isDone ? "[X] " : "[ ] ");
+    }
+
+    @Override
+    public String toString() {
+        return getSign() + description;
+    }
+
+}
